8dc6e29e2794b4632fea7d7433425295ad1f31b7
Raul Junc <juncraul@yahoo.com>
2016-10-21 10:56:24 +0100

    Added Application Peer and config files

---
+++ FirstServer/FirstApplication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Photon.SocketServer;
+using ExitGames.Logging;
+using log4net;
+using log4net.Config;
+using ExitGames.Logging.Log4Net;
+using LogManager = ExitGames.Logging.LogManager;
+
+namespace FirstServer
+{
+    public class FirstApplication : ApplicationBase
+    {
+        private static ILogger Log { get; set; }
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            Log.InfoFormat("Creating client Peer");
+            return new FirstPeer(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+            Log = LogManager.GetCurrentClassLogger();
+            GlobalContext.Properties["LogFileName"] = ApplicationName;
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(BinaryPath, "log4net.config")));
+
+            Log.InfoFormat("Setting up first custom server!");
+        }
+
+        protected override void TearDown()
+        {
+            Log.InfoFormat("Called TearDown");
+            //nothing to implement here
+        }
+    }
+}
---
+++ FirstServer/FirstPeer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace FirstServer
+{
+    public class FirstPeer : ClientPeer
+    {
+        public FirstPeer(InitRequest initRequest) : base(initRequest)
+        {
+
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            //nothing to implement here
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            //nothing to implement here
+        }
+    }
+}
